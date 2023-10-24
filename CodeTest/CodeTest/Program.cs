4d4bf72a2cee9690951ff55b0e9da7d9663e5e37
Nico Hougaard <nico@ipingroup.com>
2023-10-24 15:01:00 +0200

    Add code test, Pod calc Beta en veranderinge aan Finansiele berkeninge 2015 na verhoogde salaris inbetaal

---
+++ CodeTest/CodeTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int num = 0;
+                num = GetNextRandom();
+                Console.WriteLine(num);
+                Console.ReadLine();
+            }
+        }
+
+        private static int GetNextRandom()
+        {
+            Random r = new Random();
+            var num = r.Next();
+            return num;
+        }
+        
+    }
+}
